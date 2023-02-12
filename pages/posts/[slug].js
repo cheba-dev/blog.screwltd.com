22673fa4015aa6f24df4fbceebab7ca26af9d883
cheba-dev <91751771+cheba-dev@users.noreply.github.com>
2023-02-12 13:42:32 +0300

    AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA

--- pages/posts/[slug].js
+++ pages/posts/[slug].js
@@ -44,9 +44,17 @@ export default function PostPage({
       <Header name={globalData.name} />
       <article className="px-6 md:px-0">
         <header>
-          <h1 className="text-3xl md:text-5xl dark:text-white text-center mb-12">
+          <h1 className="text-3xl md:text-5xl dark:text-white text-center mb-3">
             {frontMatter.title}
           </h1>
+          {frontMatter.tag && (
+            <center>
+              <div className="align-middle rounded-full w-max px-2 mt-2 backdrop-blur-lg bg-white dark:bg-purple-500 dark:bg-opacity-40 bg-opacity-10 hover:bg-opacity-20 dark:hover:bg-opacity-50 transition border border-gray-800 dark:border-white border-opacity-10 dark:border-opacity-10 md:border-r-1">
+                <p className='text-sm font-sans font-thin'>{frontMatter.tag}</p>
+              </div>
+            </center>
+          )}
+          <div className='mb-3' />
           {frontMatter.description && (
             <p className="text-xl mb-4">{frontMatter.description}</p>
           )}
@@ -56,7 +64,40 @@ export default function PostPage({
             <MDXRemote {...source} components={components} />
           </article>
         </main>
-        <div className="grid md:grid-cols-2 lg:-mx-24 mt-12">
+        {frontMatter.developer && (
+          <div className='mt-4 whitespace-nowrap'>
+            <a href={'https://' + frontMatter.developer + ".screwltd.com/"}>
+              <div className="inline cursor-pointer rounded-lg w-max py-3.5 px-2 mt-2 backdrop-blur-lg bg-white dark:bg-black dark:bg-opacity-30 bg-opacity-10 hover:bg-opacity-20 dark:hover:bg-opacity-50 transition border border-gray-800 dark:border-white border-opacity-10 dark:border-opacity-10 md:border-r-1">
+                <p className="cursor-pointer center-text text-l pb-1 font-medium"><img className='cursor-pointer center-text w-9 h-9 rounded-full' src={"https://cdn.screwltd.com/developers/" + frontMatter.developer + ".png"} />&nbsp;&nbsp;{frontMatter.developer}&nbsp;</p>
+              </div>
+            </a>
+
+            {frontMatter.developer2 && (
+              <div className='inline whitespace-nowrap'>
+                &nbsp;&nbsp;&nbsp;
+                <a href={'https://' + frontMatter.developer2 + ".screwltd.com/"}>
+                  <div className="inline cursor-pointer rounded-lg w-max py-3.5 px-2 mt-2 backdrop-blur-lg bg-white dark:bg-black dark:bg-opacity-30 bg-opacity-10 hover:bg-opacity-20 dark:hover:bg-opacity-50 transition border border-gray-800 dark:border-white border-opacity-10 dark:border-opacity-10 md:border-r-1">
+                    <p className="cursor-pointer center-text text-l pb-1 font-medium"><img className='cursor-pointer center-text w-9 h-9 rounded-full' src={"https://cdn.screwltd.com/developers/" + frontMatter.developer2 + ".png"} />&nbsp;&nbsp;{frontMatter.developer2}&nbsp;</p>
+                  </div>
+                </a>
+              </div>
+            )}
+
+            {frontMatter.support && (
+              <div className='inline whitespace-nowrap'>
+                &nbsp;&nbsp;&nbsp;
+                {frontMatter.title == 'REMNANT' && (
+                  <a href='https://pages.donately.com/screwltd/campaign/remnant-7bbc8c108f5e'>
+                    <div className="inline cursor-pointer rounded-full py-2.5 px-2 mt-2 backdrop-blur-lg bg-white dark:bg-purple-500 dark:bg-opacity-40 bg-opacity-10 hover:bg-opacity-20 dark:hover:bg-opacity-50 transition border border-gray-800 dark:border-white border-opacity-10 dark:border-opacity-10 md:border-r-1">
+                      <img src='https://cdn.screwltd.com/blog/bank.png' className='w-7 h-7 inline pr-0.5 pl-0.5 pb-1 mb-0' />
+                    </div>
+                  </a>
+                )}
+              </div>
+            )}
+          </div>
+        )}
+        <div className="grid mt-6 md:grid-cols-2 lg:-mx-24 ">
           {prevPost && (
             <Link href={`/posts/${prevPost.slug}`}>
               <a className="py-8 px-10 text-center md:text-right first:rounded-t-lg md:first:rounded-tr-none md:first:rounded-l-lg last:rounded-r-lg first last:rounded-b-lg backdrop-blur-lg bg-white dark:bg-black dark:bg-opacity-30 bg-opacity-10 hover:bg-opacity-20 dark:hover:bg-opacity-50 transition border border-gray-800 dark:border-white border-opacity-10 dark:border-opacity-10 last:border-t md:border-r-0 md:last:border-r md:last:rounded-r-none flex flex-col">
@@ -94,6 +135,23 @@ export default function PostPage({
         variant="small"
         className="absolute bottom-0 opacity-20 dark:opacity-10"
       />
+      <style jsx>{`
+        .center-text {
+          vertical-align: middle;
+          display: inline;
+          position: relative;
+          top: -0.5px;
+        },
+        .dev-block {
+          height: 20px;
+          width: 20px;
+        },
+        .developers
+        {
+          display: inline;
+        }
+      `}</style>
+
     </Layout>
   );
 }
